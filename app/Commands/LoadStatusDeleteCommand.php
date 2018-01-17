1400aab71c6e7913742ecae7aa64c8305c4ae5fb
Alex Markessinis <markea125@gmail.com>
2018-01-17 13:49:30 -0500

    added load status commands

---
+++ app/Commands/LoadStatusDeleteCommand.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Commands;
+
+use OpenResourceManager\Client\LoadStatus as LoadStatusClient;
+
+class LoadStatusDeleteCommand extends APICommand
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'loads:delete {id? : The load status ID (optional)}
+                            {--c|code= : The code of the load status.}
+                            ';
+
+    /**
+     * Hide this command
+     *
+     * @var bool
+     */
+    protected $hidden = false;
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Deletes a Course by it\'s ID, or code.';
+
+    /**
+     * AddressShowCommand constructor.
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return void
+     * @throws \Exception
+     */
+    public function handle(): void
+    {
+        parent::handle();
+
+        $id = $this->argument('id');
+        $code = $this->option('code');
+
+        $client = new LoadStatusClient($this->orm);
+        $response = null;
+
+        if (empty($id)) {
+            if (empty($code)) {
+                $this->error('No identifying information found. Provide an ID argument or code option.');
+            } else {
+                $response = $client->deleteFromCode($code);
+            }
+        } else {
+            $response = $client->delete($id);
+        }
+
+        // Cache the current ORM object
+        $this->cacheORM($client->getORM());
+        $this->displayResponseBody($response);
+    }
+}
---
+++ app/Commands/LoadStatusStoreCommand.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Commands;
+
+use openresourcemanager\client\LoadStatus as LoadStatusClient;
+
+class LoadStatusStoreCommand extends APICommand
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'loads:store
+                            {code : Unique code for the load status. Must be unique across all load statuses.}
+                            {label : The load status label, this should be user friendly and easy to read.}
+                            ';
+
+    /**
+     * Hide this command
+     *
+     * @var bool
+     */
+    protected $hidden = false;
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Store Load Status information. Creates, updates, restores, a load status based on it\'s current status.';
+
+    /**
+     * AccountStoreCommand constructor.
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return void
+     * @throws \Exception
+     */
+    public function handle(): void
+    {
+        parent::handle();
+
+        $response = null;
+
+        $code = $this->argument('code');
+        $label = $this->argument('label');
+
+        $client = new LoadStatusClient($this->orm);
+
+        $response = $client->store($code, $label);
+
+        // Cache the current ORM object
+        $this->cacheORM($client->getORM());
+        $this->displayResponseBody($response);
+    }
+}
