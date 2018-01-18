d89305207972d8448edc6c2476d34a83a7af4bb9
Alex Markessinis <markea125@gmail.com>
2018-01-18 00:27:05 -0500

    set app version

--- config/app.php
+++ config/app.php
@@ -12,7 +12,7 @@ return [
     /*
      * Here goes the application version.
      */
-    'version' => 'v0.1.0-dev',
+    'version' => 'v0.1.0',
 
     /*
      * Here goes the application default command. By default
