fa54e7c12cf268f6884eadd3dd8d3bfe75c675a4
Alex Markessinis <markea125@gmail.com>
2018-07-16 12:12:44 -0400

    added support for lat/lng attrs on buildings

--- app/Commands/BuildingStoreCommand.php
+++ app/Commands/BuildingStoreCommand.php
@@ -16,6 +16,8 @@ class BuildingStoreCommand extends APICommand
                             {label : The building label, this should be user friendly and easy to read.}
                             {--campus-id= : The id of the parent campus. This is required without the --campus-code argument.}
                             {--campus-code= : The code of the parent campus. This is required without the --campus-id argument. }
+                            {--latitude= : The geo latitude of the building. }
+                            {--longitude= : The geo longitude of the building. }
                             ';
 
     /**
@@ -56,12 +58,14 @@ class BuildingStoreCommand extends APICommand
         $label = $this->argument('label');
         $campusID = $this->option('campus-id');
         $campusCode = $this->option('campus-code');
+        $lat = floatval($this->option('latitude'));
+        $long = floatval($this->option('longitude'));
         if (empty($campusCode) && empty($campusID)) {
             $this->error('Provide a campus-id or campus-code');
             die();
         }
         $client = new BuildingClient($this->orm);
-        $response = $client->store($code, $label, $campusID, $campusCode);
+        $response = $client->store($code, $label, $lat, $long, $campusID, $campusCode);
         // Cache the current ORM object
         $this->cacheORM($client->getORM());
         $this->displayResponseBody($response);
